babf2b0351f514d689dbf6e1ce371a8c28f4b95a
Lucas Clazer <lucas.clazer@gmail.com>
2020-10-15 20:15:36 -0300

    Creating report microservice module responsible for IBGE API Communication.

---
+++ ibge-report/src/main/java/com/lucas/ibgereport/IbgeReportApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.ibgereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbgeReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbgeReportApplication.class, args);
+	}
+
+}
