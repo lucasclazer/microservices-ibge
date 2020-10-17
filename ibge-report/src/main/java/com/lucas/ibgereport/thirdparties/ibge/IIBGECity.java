c0f6a2be683d71a7e2561fd41e1655ea0d78fe57
Lucas Clazer <lucas.clazer@gmail.com>
2020-10-16 22:50:42 -0300

    Implemented Feign Clients: IIBGECity, IIBGERegion;

--- ibge-report/src/main/java/com/lucas/ibgereport/thirdparties/ibge/IIBGECity.java
+++ ibge-report/src/main/java/com/lucas/ibgereport/thirdparties/ibge/IIBGECity.java
@@ -1,6 +1,6 @@
 package com.lucas.ibgereport.thirdparties.ibge;
 
-import com.lucas.ibgereport.dtos.ibge.CityDto;
+import com.lucas.ibgereport.dtos.ibge.CityDTO;
 import com.lucas.ibgereport.thirdparties.configurations.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +11,8 @@ import java.util.Collection;
 @FeignClient(name = "IIBGECity", url = "https://servicodados.ibge.gov.br", configuration = FeignConfiguration.class)
 public interface IIBGECity {
     @GetMapping(value = "api/v1/localidades/estados/{UF}/municipios")
-    public abstract Collection<CityDto> getByRegion(@PathVariable(value = "UF", required = true) String abbreviation);
+    public abstract Collection<CityDTO> getByRegion(@PathVariable(value = "UF", required = true) String abbreviation);
+
+    @GetMapping(value = "api/v1/localidades/municipios")
+    public abstract Collection<CityDTO> getCitiesFromBrazil();
 }
--- ibge-report/src/main/java/com/lucas/ibgereport/thirdparties/ibge/IIBGERegion.java
+++ ibge-report/src/main/java/com/lucas/ibgereport/thirdparties/ibge/IIBGERegion.java
@@ -1,6 +1,6 @@
 package com.lucas.ibgereport.thirdparties.ibge;
 
-import com.lucas.ibgereport.dtos.ibge.RegionDto;
+import com.lucas.ibgereport.dtos.ibge.RegionDTO;
 import com.lucas.ibgereport.thirdparties.configurations.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +10,5 @@ import java.util.Collection;
 @FeignClient(name = "IIBGERegion", url = "https://servicodados.ibge.gov.br", configuration = FeignConfiguration.class)
 public interface IIBGERegion {
     @GetMapping(value = "api/v1/localidades/estados")
-    public abstract Collection<RegionDto> getAllRegions();
+    public abstract Collection<RegionDTO> getAllRegions();
 }
