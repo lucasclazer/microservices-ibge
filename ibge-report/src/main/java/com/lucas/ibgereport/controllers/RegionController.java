262f31f227f56fe8fb318d96a2b04259c2cd98dd
Lucas Clazer <lucas.clazer@gmail.com>
2020-10-17 17:58:14 -0300

    Creating RegionController. Creating RegionService.

---
+++ ibge-report/src/main/java/com/lucas/ibgereport/controllers/RegionController.java
@@ -0,0 +1,24 @@
+package com.lucas.ibgereport.controllers;
+
+import com.lucas.ibgereport.dtos.ibge.RegionDTO;
+import com.lucas.ibgereport.services.RegionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class RegionController {
+    private RegionService regionService;
+
+    @Autowired
+    public RegionController(RegionService regionService) {
+        this.regionService = regionService;
+    }
+
+    @GetMapping(path = "regions/brazil")
+    public Collection<RegionDTO> getAllRegionsFromBrazil(){
+        return this.regionService.getAllRegions();
+    }
+}
--- ibge-report/src/main/java/com/lucas/ibgereport/services/RegionService.java
+++ ibge-report/src/main/java/com/lucas/ibgereport/services/RegionService.java
@@ -1,9 +1,25 @@
 package com.lucas.ibgereport.services;
 
+import com.lucas.ibgereport.dtos.ibge.RegionDTO;
+import com.lucas.ibgereport.thirdparties.ibge.IIBGECity;
+import com.lucas.ibgereport.thirdparties.ibge.IIBGERegion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class RegionService {
+    private final IIBGERegion iibgeRegion;
+
+    @Autowired
+    public RegionService(IIBGERegion iibgeRegion) {
+        this.iibgeRegion = iibgeRegion;
+    }
+
+    public Collection<RegionDTO> getAllRegions(){
+        return this.iibgeRegion.getAllRegions();
+    }
 
 
 }
